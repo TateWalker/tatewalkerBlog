812672a5600a306c429f5c75ea763e69bd0a1a67
Tate Walker <github@notrack.dev>
2025-03-03 02:06:09 -0600

    projects page

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,16 @@
+export const socialLinks = [
+    'github',
+    'crypto',
+    'dollar',
+    'gitlab',
+    'discord',
+    'youtube',
+    'instagram',
+    'x',
+    'telegram',
+    'rss',
+    'email',
+    'reddit',
+    'linkedin'
+  ] as const;
+  
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import { Icons } from '../assets/icons/customIcons'
+
+export type iconsType = keyof typeof Icons
+
+export { socialLinks } from '../data/socialLinks'
