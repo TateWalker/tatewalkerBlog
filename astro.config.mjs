b1903b486978ea39ae78ec304ab99224732c6159
Tate Walker <github@notrack.dev>
2025-04-01 02:27:46 -0500

    fixed copyright

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://tatewalker.com',
   // base: '/docs',
   trailingSlash: 'never',
 
