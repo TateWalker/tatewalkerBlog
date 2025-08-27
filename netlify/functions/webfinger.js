05c735d2314bc09fca19059d28e58889ac63717b
Tate Walker <github@notrack.dev>
2025-08-27 13:09:32 -0500

    webfinger

---
+++ netlify/functions/webfinger.js
@@ -0,0 +1,20 @@
+exports.handler = async (event) => {
+  const webfingerResponse = {
+    subject: "acct:@tatewalker.com",
+    links: [
+      {
+        "rel": "http://openid.net/specs/connect/1.0/issuer",
+        "href": "https://auths2wcjk6tlkg5f8ga.tatewalker.com/o/tailscale"
+      }
+    ]
+  };
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/jrd+json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(webfingerResponse)
+  };
+};
